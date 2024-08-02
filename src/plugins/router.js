16f5e6a599ceb0e9a6505bf4b4cdc85069c7cc5e
孔大夫 <kxb51812@gmail.com>
2024-08-02 12:40:17 +0800

    [init]

---
+++ src/plugins/router.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+
+// 路由信息
+const routes = [
+    {
+        path: "/",
+        name: "/",
+        component: () => import("@/views/home/index.vue"),
+    },
+
+];
+
+// 导出路由
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      imports: ["vue", "vue-router"],
+  }),
+  Components({
+      resolvers: [ElementPlusResolver()],
+  }),
+  ],
+  resolve: {
+    alias: [
+        {
+            find: "@",
+            replacement: resolve(__dirname, "src"),
+        },
+    ],
+},
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
